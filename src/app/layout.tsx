bb7a3de71b287d9a76849735a2731359d8d3b476
Jishu Das <jishudas329@gmail.com>
2025-12-17 11:43:19 +0000

    remove the back to top button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,6 @@ import Footer from '@/components/footer';
 import { FirebaseClientProvider } from '@/firebase';
 import AuthProvider from '@/components/auth-provider';
 import Loader from '@/components/loader';
-import BackToTopButton from '@/components/back-to-top-button';
 
 export const metadata: Metadata = {
   title: 'Drift Dynamic',
@@ -44,7 +43,6 @@ export default function RootLayout({
               </div>
               <CartDrawer />
               <Toaster />
-              <BackToTopButton />
             </AuthProvider>
           </AppContextProvider>
         </FirebaseClientProvider>
