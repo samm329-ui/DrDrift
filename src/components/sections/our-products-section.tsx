6936983d7e1fdcd44d602e39c3d4ee5f11e02ee6
Jishu Das <jishudas329@gmail.com>
2025-12-17 11:27:55 +0000

    remove the arrow from the button and just keep buy now in the our produc

--- src/components/sections/our-products-section.tsx
+++ src/components/sections/our-products-section.tsx
@@ -166,10 +166,6 @@ const ProductCard = ({ product }: { product: SiteProduct }) => {
                 <AddToCartButton onClick={handleAddToCart} className="flex-grow basis-0 justify-center" />
                 <button onClick={handleBuyNow} className="hero-buy-now-btn flex-grow basis-0 justify-center text-black">
                   <span>Buy Now</span>
-                   <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="none">
-                    <path d="M0 0h24v24H0V0z" fill="none"/>
-                    <path d="M16.01 11H4v2h12.01v3L20 12l-3.99-4v3z" fill="currentColor"/>
-                  </svg>
                 </button>
             </div>
           </CardFooter>
